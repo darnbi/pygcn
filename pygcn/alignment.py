1bdd86a29e04f15ba6343daefaae2c3873be8d8f
darnbi <923214@naver.com>
2019-10-05 00:47:23 +1000

    Add existing file

---
+++ pygcn/alignment.py
@@ -0,0 +1,33 @@
+import time
+import argparse
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+# calculate alignment matrix with dot product which computes similarity
+def alignment(embed_1, embed_2):
+
+    node_num1 = embed_1.shape[0]
+    node_num2 = embed_2.shape[0]
+    i = 0
+    j = 0
+    
+    #rank = [[0] * node_num1] * node_num2
+    rank = np.zeros((node_num1, node_num2))
+    
+    for row1 in embed_1:
+        j = 0
+        for row2 in embed_2:
+            
+            #get the similarity between current node in embed_1 and all the node in embed_2
+            rank[i, j] = np.dot(row1, row2)
+            j = j+1
+        i = i+1
+    #return final alignment matrix       
+    return rank
+
+
+
